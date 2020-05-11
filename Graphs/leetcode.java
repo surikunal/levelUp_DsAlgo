768bb6e9afab51f8e52c8a029736e6d60bf81a2c
kunal <suri.kunal.222@gmail.com>
2020-05-12 02:21:52 +0530

    leetcode

--- Graphs/leetcode.java
+++ Graphs/leetcode.java
@@ -1,4 +1,8 @@
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 // 130. ===========================================================
 
@@ -140,9 +144,59 @@ class Solution3 {
         }
         return count;
     }
+
 }
 
 // ! cpp have some extra question so pls kindly check there
+
+// leetcode 815.=======================================================
+class Solution6 {
+    public int numBusesToDestination(int[][] routes, int S, int T) {
+        if (routes.length == 0)
+            return -1;
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        for (int i = 0; i < routes.length; i++) {
+            for (int ele : routes[i]) {
+                map.putIfAbsent(ele, new ArrayList<>());
+                map.get(ele).add(i);
+            }
+        }
+
+        HashSet<Integer> busStandVis = new HashSet();
+        boolean[] busVis = new boolean[routes.length];
+
+        LinkedList<Integer> que = new LinkedList<>();
+        que.addLast(S);
+        int level = 0;
+        busStandVis.add(S);
+
+        while (que.size() != 0) {
+            int size = que.size();
+            while (size-- > 0) {
+                int stand = que.removeFirst();
+
+                if (stand == T)
+                    return level;
+
+                for (int bus : map.get(stand)) {
+                    if (busVis[bus])
+                        continue;
+
+                    for (int busStand : routes[bus]) {
+                        if (!busStandVis.contains(busStand)) {
+                            que.addLast(busStand);
+                            busStandVis.add(busStand);
+                        }
+                    }
+                    busVis[bus] = true;
+                }
+            }
+            level++;
+        }
+
+        return -1;
+    }
+}
 
 // 684. =====================================================
 
