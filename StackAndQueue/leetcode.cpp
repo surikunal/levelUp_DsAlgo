ca03779581f087b44305167b9a320a7d34cc45c9
kunal <suri.kunal.222@gmail.com>
2020-06-20 23:25:06 +0530

    update

--- StackAndQueue/leetcode.cpp
+++ StackAndQueue/leetcode.cpp
@@ -145,3 +145,45 @@ public:
         return ans;
     }
 };
+
+// 735. ======================================================
+class Solution
+{
+public:
+    vector<int> asteroidCollision(vector<int> &arr)
+    {
+        stack<int> st;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            if (arr[i] > 0)
+            {
+                st.push(arr[i]);
+            }
+            else
+            {
+                while (st.size() != 0 && st.top() > 0 && st.top() < -arr[i])
+                {
+                    st.pop();
+                }
+
+                if (st.size() != 0 && st.top() == -arr[i])
+                {
+                    st.pop();
+                }
+
+                else if (st.size() == 0 || st.top() < 0)
+                {
+                    st.push(arr[i]);
+                }
+            }
+        }
+        vector<int> ans(st.size(), 0);
+        int k = st.size() - 1;
+        while (st.size() != 0)
+        {
+            ans[k--] = st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
